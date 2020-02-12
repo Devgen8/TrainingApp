8aede0dcb40bc5a42c1c263c75a62a4e0885963d
Evgeniy Kamaev <zhecka.kamaev@yandex.ru>
2020-02-12 13:03:40 +0300

    Initial Commit

---
+++ TrainingApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TrainingApp
+//
+//  Created by мак on 12/02/2020.
+//  Copyright © 2020 мак. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
