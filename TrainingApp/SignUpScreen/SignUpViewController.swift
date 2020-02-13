3908be39fca6c5e2d7145e35464ddc83a636fde7
Evgeniy Kamaev <zhecka.kamaev@yandex.ru>
2020-02-13 14:17:23 +0300

    Sign Up screen added

---
+++ TrainingApp/SignUpScreen/SignUpViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SignUpViewController.swift
+//  TrainingApp
+//
+//  Created by мак on 13/02/2020.
+//  Copyright © 2020 мак. All rights reserved.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
